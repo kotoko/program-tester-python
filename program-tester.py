d9cb362773b4c648ceb85573b9157bb0ab7c9990
kotoko <kotoko@users.noreply.github.com>
2017-04-12 19:26:45 +0200

    Fix determining script's directory when run from symlink

--- program-tester.py
+++ program-tester.py
@@ -19,7 +19,7 @@ app = "program-tester"
 gettext.bindtextdomain(
 		app,
 		os.path.join(
-			os.path.abspath(os.path.dirname(__file__)),
+			os.path.dirname(os.path.realpath(__file__)),
 			"l10n"
 		)
 	)
